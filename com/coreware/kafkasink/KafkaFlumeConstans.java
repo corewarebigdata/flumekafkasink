f8caf17935814da188ab7568059fccbaca374781
corewarebigdata <freeaya1@gmail.com>
2014-06-25 17:40:43 +0800

    Revert "Revert "newer""
    This reverts commit 80efb016e540fcdfd9ca4a40b4f955ee7b8b4329.

--- com/coreware/kafkasink/KafkaFlumeConstans.java
+++ com/coreware/kafkasink/KafkaFlumeConstans.java
@@ -19,26 +19,14 @@ package org.apache.flume.plugins;
 
 public class KafkaFlumeConstans {
 
-    /**
-     * The constant PARTITION_KEY_NAME.
-     */
     public static final String PARTITION_KEY_NAME = "custom.partition.key";
-    /**
-     * The constant ENCODING_KEY_NAME.
-     */
+    
     public static final String ENCODING_KEY_NAME = "custom.encoding";
-    /**
-     * The constant DEFAULT_ENCODING.
-     */
+    
     public static final String DEFAULT_ENCODING = "UTF-8";
-    /**
-     * The constant CUSTOME_TOPIC_KEY_NAME.
-     */
+    
     public static final String CUSTOME_TOPIC_KEY_NAME = "custom.topic.name";
-
-    /**
-     * The constant CUSTOME_TOPIC_KEY_NAME.
-     */
+    
     public static final String CUSTOME_CONSUMER_THREAD_COUNT_KEY_NAME = "custom.thread.per.consumer";
     
     public static final String CUSTOM_BATCH_SIZE = "custom.batch-size";
